8beadfa05b24e5f278bad5ca3c8cff5760b375bb
Joshua Gagnon <josh.n.gagnon@gmail.com>
2016-08-01 11:02:58 +1200

    now shows features without js, added gc document blurb

--- src/js/app.js
+++ src/js/app.js
@@ -1,14 +1,13 @@
 import '../style/style.scss';
 import $ from 'jquery';
-import  'jquery-inview';
+import 'jquery-inview';
 import 'bootstrap/js/transition';
 import 'bootstrap/js/dropdown';
 import 'bootstrap/js/collapse';
 
-console.log(jQuery.fn.collapse())
-
 
 if($('.scroll-row').length) {
+    $('.scroll-row .fade-container').addClass('animate');
     $('.scroll-row .fade-container').bind('inview', function(event, isInView, visiblePartX, visiblePartY) {
 
         if(isInView) {
